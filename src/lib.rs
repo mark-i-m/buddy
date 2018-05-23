df4cb9270dad63c2c873d4c2af6f10feaba722fb
Mark Mansi <markm@cs.wisc.edu>
2018-05-23 14:42:34 -0500

    add simple test

--- src/lib.rs
+++ src/lib.rs
@@ -56,6 +56,8 @@ where
     }
 
     /// Add the given range to the buddy allocator. `start` _and_ `end` are inclusive.
+    /// TODO: maybe this has to be contiguous? Otherwise, how do we tell if there is a whole? Maybe
+    /// by size of allocations?
     pub fn extend(&mut self, start: T, end: T) {
         //TODO: recursively decompose into powers of two.
         //TODO: split large block sizes into size of last bin.
@@ -176,6 +178,63 @@ mod test {
 
     #[test]
     fn test_simple() {
-        let a = BuddyAllocator::new(0, 500, 9);
+        let mut a = BuddyAllocator::new(0, 511, 10);
+
+        // check initial state
+        assert_eq!(a.bins.len(), 10);
+        assert_eq!(a.bins[9].len(), 1);
+        assert!(a.bins[9].contains(&0));
+        for i in 0..9 {
+            assert_eq!(a.bins[i].len(), 0);
+        }
+
+        // allocate 1
+        let alloced = a.alloc(1);
+        assert_eq!(alloced, Some(0));
+
+        // check state after alloc
+        assert_eq!(a.bins[9].len(), 0);
+
+        assert_eq!(a.bins[8].len(), 1);
+        assert!(a.bins[8].contains(&256));
+
+        assert_eq!(a.bins[7].len(), 1);
+        assert!(a.bins[7].contains(&128));
+
+        assert_eq!(a.bins[6].len(), 1);
+        assert!(a.bins[6].contains(&64));
+
+        assert_eq!(a.bins[5].len(), 1);
+        assert!(a.bins[5].contains(&32));
+
+        assert_eq!(a.bins[4].len(), 1);
+        assert!(a.bins[4].contains(&16));
+
+        assert_eq!(a.bins[3].len(), 1);
+        assert!(a.bins[3].contains(&8));
+
+        assert_eq!(a.bins[2].len(), 1);
+        assert!(a.bins[2].contains(&4));
+
+        assert_eq!(a.bins[1].len(), 1);
+        assert!(a.bins[1].contains(&2));
+
+        assert_eq!(a.bins[0].len(), 1);
+        assert!(a.bins[0].contains(&1));
+
+        // free allocation
+        a.free(0, 1);
+
+        // check final state
+        assert_eq!(a.bins.len(), 10);
+        assert_eq!(a.bins[9].len(), 1);
+        assert!(a.bins[9].contains(&0));
+        for i in 0..9 {
+            assert_eq!(a.bins[i].len(), 0);
+        }
     }
+
+    // TODO: test with small number of bins
+
+    // TODO: test extend
 }
